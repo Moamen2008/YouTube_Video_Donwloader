5707c119d6f2c7b55d6932fc27cb757fc7c702a8
Moamen <Moamen2008@outlook.com>
2023-09-06 00:38:13 +0300

    Vesrion V0.3

--- YouTube_Video_Downlaoder.py
+++ YouTube_Video_Downlaoder.py
@@ -1,35 +1,102 @@
 from pytube import YouTube
+import time
 
-print("Made by Moamen Ashraf\nVideo Downloader V0.1")
+print("Made by Moamen Ashraf \nVideo Downloader V0.3")
 
-link = str(input("Paste Video link over here: "))
-yt = YouTube(link)
-Res = str(input("res of the video (144p,360p,720p,audio_only) :"))
+def main():
 
-print("Title: ", yt.title)
-print("Views: ", yt.views)
+     link = str(input("Paste Video link over here: "))
+     yt = YouTube(link)
+     Res = str(input("Res of the Video ( 144p, 360p, 720p, Audio ) ? : "))
+          
+
+     
+     print("Title: ", yt.title)
+     print("Author: ", yt.author)
+     print("Views: ", yt.views)
+     print("Time of the video in minutes : ", str(float(yt.length)/60))
+
+     def video_donwloading():
+          print("**********!Donwloading!**********")
+     if Res == "144p":
+          yt.streams.get_lowest_resolution().download("F:\YoutubeDownload\\144p")
+          print("Done!")
+     elif Res == "360p":
+          yt.streams.get_by_resolution("360p").download("F:\YoutubeDownload\\360p")
+          print("Done!")
+     elif Res == "720p":
+          yt.streams.get_highest_resolution().download("F:\YoutubeDownload\\720p")
+          print("Done!")
+     elif Res.lower() == "audio_only" or "audio":
+          yt.streams.filter(only_audio=True).first().download("F:\YoutubeDownload\\Audio", filename=f"{yt.title}.mp3" )
+          print("Done!")
+     else:
+          print("Wrong Input")
+     
 
 
+     if Res == "144p":
+          yt_size = yt.streams.get_lowest_resolution().filesize_mb
+          print("video-size= ", yt_size,"MB")
+          play_again = input("Do you want to download the video? (y/n): ")
+          if play_again.lower() == "y":
+               video_donwloading()
+          if play_again.lower() == "n":
+               print("thanks for using my program") 
+               
+     elif Res == "240p":
+          print("Res not Supported")
 
-if Res == "144p":
-   video = yt.streams.get_lowest_resolution()
-   yt_size = yt.streams.get_lowest_resolution().filesize_mb
-   print("video-size= ", yt_size)
-   ask_downloading = str(input("Do you Want To download the video (y/n)? : "))
-   if ask_downloading.lower == "y":
-       print("**********!Donwloading!**********")
-       video.download()
-   else:
-       print("thanks for using my program")
-elif Res == "240p":
-    print("Res not Supported")
-elif Res == "360p":
-    yt.streams.get_by_resolution("360p").download("Users\Moame\Videos\Pytube\360p")
-elif Res == "480p":
-    print("Res not Supported")
-elif Res == "720p":
-    yt.streams.get_highest_resolution().download()
-elif Res.lower() == "audio_only" or "audio":
-    yt.streams.get_audio_only().download()
+     elif Res == "360p":
+          yt_size = yt.streams.get_lowest_resolution().filesize_mb
+          print("video-size= ", yt_size,"MB")
+          play_again = input("Do you want to download the video? (y/n): ")
+          if play_again.lower() == "y":
+               video_donwloading()
+          if play_again.lower() == "n":
+               print("thanks for using my program") 
 
-print("Done!")
+     elif Res == "480p":
+          print("Res not Supported")
+
+     elif Res == "720p":
+          yt_size = yt.streams.get_highest_resolution().filesize_mb
+          print("video-size= ", yt_size,"MB")
+          play_again = input("Do you want to download the video? (y/n): ")
+          if play_again.lower() == "y":
+               video_donwloading()
+          if play_again.lower() == "n":
+               print("thanks for using my program") 
+
+     elif Res.lower() == "audio_only":
+          yt_size = yt.streams.get_audio_only().filesize_mb
+          print("video-size= ", yt_size,"MB")
+          play_again = input("Do you want to download the video? (y/n): ")
+          if play_again.lower() == "y":
+               video_donwloading()
+          if play_again.lower() == "n":
+               print("thanks for using my program") 
+
+     elif Res.lower() == "audio":
+          yt_size = yt.streams.get_audio_only().filesize_mb
+          print("video-size= ", yt_size,"MB")
+          play_again = input("Do you want to download the video? (y/n): ")
+          if play_again.lower() == "y":
+               video_donwloading()
+          if play_again.lower() == "n":
+               print("thanks for using my program") 
+     else:
+          print("Wrong Input")
+
+
+def main_use():
+     main()
+     re_use = input("Do You Want To Download Another Video (y/n)? : ")
+     if re_use == "y":
+          main_use()
+     elif re_use == "n":
+          print("Thank You For Using My Program")
+          time.sleep(3)
+          quit()
+     
+main_use()
